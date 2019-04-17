5b330b57c96ba3818963dc365d5b3364cd21603e
DAVID ne DEVID <god.of.cplpl@gmail.com>
2019-04-17 14:31:38 +0300

    done smth with brackets an done the simple calculator

--- CALC Progect/Calculator.cpp
+++ CALC Progect/Calculator.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-int calculator(string algebr);
-bool check_hight_pr(vector<char> a);
+double calculator(string algebr);
 
 ostream& operator<<(ostream& cout, vector<int>& vec)
 {
@@ -22,18 +21,25 @@ ostream& operator<<(ostream& cout, vector<int>& vec)
 int main()
 {
 	string a;
-	/*getline(cin, a);*/
-	cout << calculator("78/6*3-6*5+2/6*2-6/3*2") << endl;
+	getline(cin, a);
+	system("cls");
+	cout << a << " = " << calculator(a) << endl;
 	_getch();
 }
 
 
-int calculator(string algebr)
+double calculator(string algebr)
 {
-	vector<int> numbers;
+	vector<double> numbers; 
 	vector<char> symbols;
-	int L_solution = 0;
-	int H_solution = 0;
+	vector<double> splited_numbers;
+	vector<char> splited_symbols;
+
+	double L_solution = 0;
+	double H_solution = 0;
+
+	bool error_scobe = false;
+	int col_scob = 0;
 
 	for (int i = 0; i <= algebr.size(); i++)
 	{
@@ -42,19 +48,55 @@ int calculator(string algebr)
 		{
 			continue;
 		}
-		for (i; algebr[i] >= 48 && algebr[i] <= 57; i++)
+		for (i; algebr[i] >= 48 && algebr[i] <= 57 || algebr[i] == 46; i++)
 		{
 			temp_number += algebr[i];
 		}
+		if (algebr[i] == '(')
+		{
+			error_scobe = true;
+			col_scob++;
+		}
+		else if (algebr[i] == ')')
+		{
+			if (error_scobe)
+			{
+				error_scobe = false;
+			}
+			else
+			{
+				error_scobe = true;
+			}
+			col_scob++;
+		}
 		if (temp_number != "")
 		{
-			numbers.push_back(stoi(temp_number));
+			numbers.push_back(stod(temp_number));
 		}
-		if (algebr[i] != ' ' && (algebr[i] == '+' || algebr[i] == '-' || algebr[i] == '*' || algebr[i] == '/' || algebr[i] == '(' || algebr[i] == ')'))
+		if (algebr[i] != ' ' && (algebr[i] == '+' || algebr[i] == '-' || algebr[i] == '*' || algebr[i] == '/'/* || algebr[i] == '(' || algebr[i] == ')'*/))
 		{
 			symbols.push_back(algebr[i]);
 		}
+		else if (algebr[i] == '=')
+		{
+			cout << "ERROR" << endl;
+			return 0;
+		}
 	}
+
+	// CHECK
+
+	if((symbols.size() - col_scob) >= numbers.size() || (error_scobe == true) && col_scob % 2 == 0)
+	{
+		cout << "ERROR" << endl;
+		return 0;
+	}
+	else if (numbers.size() == 1)
+	{
+		return numbers[0];
+	}
+
+	// CHECK
 
 	for (int i = 0; i < symbols.size(); i++)
 	{
@@ -100,7 +142,6 @@ int calculator(string algebr)
 
 	// CORRECTING
 
-	 // 78/6*3-6*5+2/6*2-6/3*2
 	for (int i = symbols.size() - 1; i >= 0; i--)
 	{
 		if (symbols[i] == '*' || symbols[i] == '/')
@@ -108,46 +149,61 @@ int calculator(string algebr)
 			numbers[i] = numbers[i + 1];
 			H_solution = numbers[i];
 		}
+
 	}
 
 	// CORRECTING
 
-	bool check = false;
 
-	for (int i = 0; i < symbols.size(); i++)
+	// SPLITTING TO LOW PRIORITTY
+
+	if (symbols.size() > 1)
 	{
-		switch (symbols[i])
+		for (int i = 0; i < symbols.size(); i++)
 		{
-		case '-':
-			numbers[i] -= numbers[i + 1];
-			numbers[i + 1] = numbers[i];
-			L_solution = numbers[i + 1];
-			check = true;
-			break;
-		case '+':
-			numbers[i] += numbers[i + 1];
-			numbers[i + 1] = numbers[i];
-			L_solution = numbers[i + 1];
-			check = true;
-			break;
+			if (symbols[i] == '+' || symbols[i] == '-')
+			{
+				splited_numbers.push_back(numbers[i]);
+				if (i == symbols.size() - 2)
+				{
+					splited_numbers.push_back(numbers[i + 1]);
+				}
+				splited_symbols.push_back(symbols[i]);
+
+			}
 
 		}
 	}
 
-	for (int i = 0; i < symbols.size(); i++)
+	else
 	{
-		switch (symbols[i])
+				splited_numbers.push_back(numbers[0]);
+				splited_numbers.push_back(numbers[1]);
+				splited_symbols.push_back(symbols[0]);
+
+	}
+
+	numbers.clear();
+	symbols.clear();
+
+	// SPLITTING TO LOW PRIORITTY
+
+	bool check = false;
+
+	for (int i = 0; i < splited_symbols.size(); i++)
+	{
+		switch (splited_symbols[i])
 		{
 		case '-':
-			numbers[i] -= numbers[i + 1];
-			numbers[i + 1] = numbers[i];
-			L_solution = numbers[i + 1];
+			splited_numbers[i] -= splited_numbers[i + 1];
+			splited_numbers[i + 1] = splited_numbers[i];
+			L_solution = splited_numbers[i + 1];
 			check = true;
 			break;
 		case '+':
-			numbers[i] += numbers[i + 1];
-			numbers[i + 1] = numbers[i];
-			L_solution = numbers[i + 1];
+			splited_numbers[i] += splited_numbers[i + 1];
+			splited_numbers[i + 1] = splited_numbers[i];
+			L_solution = splited_numbers[i + 1];
 			check = true;
 			break;
 
@@ -162,16 +218,4 @@ int calculator(string algebr)
 	{
 		return H_solution;
 	}
-}
-
-bool check_hight_pr(vector<char> a)
-{
-	for (int i = 0; i < a.size(); i++)
-	{
-		if (a[i] == '*' || a[i] == '/')
-		{
-			return true;
-		}
-	}
-	return false;
 }
